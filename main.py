ef35e9cbe5abdeeb94f9fdb948bf8e02822eadf8
Caspermun <caspermunele@gmail.com>
2021-10-27 23:29:51 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,69 @@
+# 1
+# for i in range(1, 11):
+#         print(3, '*', i, '=', 3 * i)
+
+# 2
+# a = (int(input('Введите число: ')) + 1)
+# b = 0
+# for i in range(1, a):
+#     b += i
+# print(b)
+
+# 3
+# b = 1
+# a = [1, 2, 3, 4, 5]
+# for i in a:
+#     b *= i
+# print(b)
+
+# 4
+# obj = {'Минск': 'Беларусь', 'Москва': 'Россия', 'Киев': 'Украина'}
+# for i in obj:
+#     print(i, '- это', obj[i])
+
+# 5
+# a = 0
+# while a < 100:
+# 	a += 1
+# 	print(a)
+
+# 5
+# for i in range(1, 101):
+# 	print(i)
+
+# 6
+# a = 10
+# while a < 33:
+# 	a += 1
+# 	print(a)
+
+# 6
+# for i in range(11, 34):
+# 	print(i)
+
+# 7
+# a = 0
+# while a < 100:
+# 	a += 2
+# 	print(a)
+
+# 7
+# for i in range(0, 101, 2):
+# 	print(i)
+
+# 8
+# a = 0
+# b = 0
+# while a < 101:
+#     b += a
+#     a += 1
+# print(b)
+
+# 8
+# a = 0
+# for i in range(1, 101):
+# 	a += i
+# print(a)
+
+if __name__ == '__main__':
+    pass
